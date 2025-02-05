81d3fe1585a88d1c3bfcd0f3465ad443c7ee53fe
Usman <Cap.usman21@gmail.com>
2025-02-05 17:19:04 +0500

    written tests for un and autheticated user, views and url paths

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,7 +1,7 @@
 """Serializer for Recipe APIs."""
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -17,3 +17,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for Tags"""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
